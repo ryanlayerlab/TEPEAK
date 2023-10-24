73c9fc6d519bb71c8162acfe35320884436e136f
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-10-24 11:18:54 -0600

    created test script for bash scripts

--- test/test_tepeaks.sh
+++ test/test_tepeaks.sh
@@ -1,7 +1,7 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-rm -rf config_ecoli.yaml
+rm -rf configs/config_ecoli.yaml
 
 run test_species_start_config bash species_start_config.sh -s ecoli -d data -n 1
 #test that data_dir is made 
@@ -12,7 +12,7 @@ fi
 assert_equal "true" "$data_exists"
 # assert_equal true $(-d "data")
 
-#test that a specices sub dir is made
+#test that a data_dir/species_dir is made
 species_exists=false
 if [ -d "data/ecoli" ]; then 
     species_exists=true
@@ -20,13 +20,13 @@ fi
 assert_equal "true" "$species_exists"
 
 #test that a config file is made 
-assert_equal config_ecoli.yaml $(ls config_ecoli.yaml)
+assert_equal configs/config_ecoli.yaml $(ls configs/config_ecoli.yaml)
 #test that "species: $species" is in  $config_file
-assert_equal "ecoli" $(cat config_ecoli.yaml | grep species | cut -d " " -f 2)
+assert_equal "ecoli" $(cat configs/config_ecoli.yaml | grep species | cut -d " " -f 2)
 #test that "data_directory: $data_dir" is in $config_file
-assert_equal "data" $(cat config_ecoli.yaml | grep data_directory | cut -d " " -f 2)
+assert_equal "data" $(cat configs/config_ecoli.yaml | grep data_directory | cut -d " " -f 2)
 #test that "threads: $threads" is in $config_file
-assert_equal "1" $(cat config_ecoli.yaml | grep thread | cut  -d " " -f 2)
+assert_equal "1" $(cat configs/config_ecoli.yaml | grep thread | cut  -d " " -f 2)
 
 
 run test_get_sra_numbers python get_sra_numbers.py -f SraRunTable.txt -n 4 -s ecoli
@@ -34,3 +34,16 @@ assert_equal "ERR10355883" $(cat data/ecoli/ecoli_samples.txt | grep ERR10355883
 assert_equal "ERR10355891" $(cat data/ecoli/ecoli_samples.txt | grep ERR10355891)
 assert_equal "ERR10355911" $(cat data/ecoli/ecoli_samples.txt | grep ERR10355911)
 assert_equal "ERR10355944" $(cat data/ecoli/ecoli_samples.txt | grep ERR10355944)
+
+run test_process_reference.sh bash process_reference.sh -s ecoli -f data/ncbi_dataset.zip 
+# run test_process_reference.sh python process_reference.py -s ecoli -f data/ncbi_dataset.zip 
+# testing the creation of files innside data_dir/species_dir
+assert_equal data/ecoli/ecoli.fa $(ls data/ecoli/ecoli.fa)
+assert_equal data/ecoli/ecoli.amb $(ls data/ecoli/ecoli.amb)
+assert_equal data/ecoli/ecoli.ann $(ls data/ecoli/ecoli.ann)
+assert_equal data/ecoli/ecoli.bwt $(ls data/ecoli/ecoli.bwt)
+assert_equal data/ecoli/ecoli.fa.fai $(ls data/ecoli/ecoli.fa.fai)
+assert_equal data/ecoli/ecoli.pac $(ls data/ecoli/ecoli.pac)
+assert_equal data/ecoli/ecoli.sa $(ls data/ecoli/ecoli.sa)
+
+# testing the contents of files created inside data_dir/species_dir
