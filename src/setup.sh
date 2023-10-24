152385ca2bbfb25d567afc0be087f938c2410bcd
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-10-24 12:04:33 -0600

    added setup.sh

---
+++ src/setup.sh
@@ -0,0 +1,9 @@
+conda config --append channels bioconda
+conda install insurveyor
+conda install -c bioconda bedtools
+conda install -c conda-forge selenium
+conda install -c bioconda bcftools
+conda install -c bioconda bwa
+conda install -c bioconda samtools
+conda install jq
+pip install -r requirements.txt
