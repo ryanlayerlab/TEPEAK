ea90857a2363e76488035b98d1c0317e36e275f2
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-10-25 13:00:17 -0600

    switched os.system for subprocess.call

--- src/process_reference.py
+++ src/process_reference.py
@@ -1,5 +1,5 @@
 import argparse
-from os import system as os_system
+from subprocess import call
 from json import load as json_load
 from yaml import safe_load as yaml_safe_load
 
@@ -14,7 +14,7 @@ def main():
     filename, species = args.filename, args.species
     
     # unzip the file and auto input 'no' to replace README prompt
-    os_system(f'echo "n" | unzip {filename}')
+    call(f'echo "n" | unzip {filename}')
     with open('ncbi_dataset/data/dataset_catalog.json') as dataset_catalog, open(f'configs/config_{species}.yaml') as config_file: 
         parsed_dataset = json_load(dataset_catalog)
         config_file = yaml_safe_load(config_file)
@@ -22,7 +22,7 @@ def main():
     data_dir = config_file['data_directory']
     fa_filepath = f"ncbi_dataset/data/{parsed_dataset['assemblies'][2]['files'][0]['filePath']}"
 
-    os_system(
+    call(
         f"""
         cp {fa_filepath} {data_dir}/{species}/{species}.fa
         echo "File has been moved and renamed to {species}.fa"
