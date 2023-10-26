64b0213765854dc21226d8513d3f3e353904e413
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-10-26 14:43:44 -0600

    fixed error where space in directory would crash script

--- src/align_species.sh
+++ src/align_species.sh
@@ -27,8 +27,7 @@ while IFS= read -r line; do
 
     line=$(echo "$line" | tr -d '[:space:]')
     sra_example=$line
-    # pwd
-    cd $data_dir/$species
+    cd "$data_dir/$species"
 
     mkdir -p $sra_example
     mkdir -p "${sra_example}"/bwa_errors
@@ -38,19 +37,15 @@ while IFS= read -r line; do
     prefetch "${sra_example}" --max-size 200G
     fastq-dump --split-files "${sra_example}"
 
-    # echo $data_path
-
     bwa mem -M -t $threads -R "@RG\tID:1\tSM:""${sra_example}" \
-         $data_path/$species \
+         "$data_path/$species" \
          "${sra_example}"_1.fastq "${sra_example}"_2.fastq  \
          2> bwa_errors/bwa_"${sra_example}".err \
         > "${sra_example}".bam
 
-
     samtools sort "${sra_example}".bam -o "${sra_example}".sorted.bam #-@$threads
 
     samtools index "${sra_example}".sorted.bam #-@$threads
-
 
     java -jar "$picard_path"/picard/build/libs/picard.jar FixMateInformation \
    	I="${sra_example}".sorted.bam \
@@ -58,8 +53,8 @@ while IFS= read -r line; do
     	O="${sra_example}".fixed.bam  \
     	TMP_DIR="$(pwd)/tmp"
 
-     samtools index "${sra_example}".fixed.bam
- 
+    samtools index "${sra_example}".fixed.bam
+
     mv "${sra_example}".fixed.bam ../"${sra_example}".bam
     mv "${sra_example}".fixed.bam.bai ../"${sra_example}".bam.bai
     
@@ -74,8 +69,7 @@ while IFS= read -r line; do
     #rm "${sra_example}"_2.fastq
     #rm  -r tmp 
     
-    # echo $data_path
-    cd $data_path
+    cd "$data_path"
     cd ../..
   
 done < "$sra_file"
