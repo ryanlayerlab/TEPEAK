e108e3ad0ca638c9231c01c0c9ea69108514cf77
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-10-26 14:45:04 -0600

    changed subprocess.call back to os.system

--- src/process_reference.py
+++ src/process_reference.py
@@ -1,5 +1,5 @@
 import argparse
-from subprocess import call
+from os import system as os_system
 from json import load as json_load
 from yaml import safe_load as yaml_safe_load
 
@@ -14,15 +14,14 @@ def main():
     filename, species = args.filename, args.species
     
     # unzip the file and auto input 'no' to replace README prompt
-    call(f'echo "n" | unzip {filename}')
+    os_system(f'echo "n" | unzip {filename}')
     with open('ncbi_dataset/data/dataset_catalog.json') as dataset_catalog, open(f'configs/config_{species}.yaml') as config_file: 
         parsed_dataset = json_load(dataset_catalog)
         config_file = yaml_safe_load(config_file)
     
     data_dir = config_file['data_directory']
     fa_filepath = f"ncbi_dataset/data/{parsed_dataset['assemblies'][2]['files'][0]['filePath']}"
-
-    call(
+    os_system(
         f"""
         cp {fa_filepath} {data_dir}/{species}/{species}.fa
         echo "File has been moved and renamed to {species}.fa"
@@ -32,6 +31,10 @@ def main():
         bwa index -p {species} {species}.fa
         """
     )
+
+    ## tried using subprocess.run instead of os.system for all bash commands. it was more hassle than needed. 
+    ## doesn't work well with f"" and multiple commands
+    ## maybe if i moved the multiline commands into a seperate bash script and called the script using subprocess.run
 
 if __name__ == "__main__":
     main()
