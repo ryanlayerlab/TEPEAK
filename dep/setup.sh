51529592918d0a86c0bfc6b7d40ae7867104174d
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-10-31 14:56:23 -0600

    removed extra conda install insurveyor

--- dep/setup.sh
+++ dep/setup.sh
@@ -1,8 +1,8 @@
 conda config --append channels bioconda
-conda install insurveyor
+conda config --append channels conda-forge
 conda install -c bioconda bedtools
 conda install -c conda-forge selenium
-conda install -c bioconda bcftools
+conda install -c bioconda -c conda-forge bcftools
 conda install -c bioconda bwa
 conda install -c bioconda samtools
 conda install jq
