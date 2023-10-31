74937e3581bcf1a4d1f764340cfbf9c4dcd86d8a
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-10-31 15:39:24 -0600

    made nicer to look at

--- src/build_histogram.py
+++ src/build_histogram.py
@@ -1,51 +1,57 @@
-import argparse
+from argparse import ArgumentParser
 import pandas as pd
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser(description='Process some arguments.')
-parser.add_argument('-f', '--pop_vcf_file', required=True, help='Population VCF file')
-parser.add_argument('-l', '--lower', required=False, help='lower range')
-parser.add_argument('-u', '--upper', required=False, help='upper range') 
+def parse_args():
+	parser = ArgumentParser(description='Process some arguments.')
+	parser.add_argument('-f', '--pop_vcf_file', required=True, help='Population VCF file')
+	parser.add_argument('-l', '--lower', required=False, help='lower range')
+	parser.add_argument('-u', '--upper', required=False, help='upper range') 
+	return parser.parse_args()
 
-args = parser.parse_args()
-
-
-if args.lower is None:
-	args.lower = 0
-
-if args.upper is None:
-	args.upper = 10000
+def main():
+	args = parse_args()
 
 
-min_size = str(args.lower)
-max_size = str(args.upper)
-sv_info_file = args.pop_vcf_file
+	if args.lower is None:
+		args.lower = 0
 
-df = pd.read_csv(sv_info_file, sep='\t', lineterminator='\n')
-df.columns = ['chrom','start','end','length','seq']
+	if args.upper is None:
+		args.upper = 10000
 
-df['length'].value_counts()
-df = df[df['length']!='.']
 
-#### if you get NaN error run this first line and then delete whatever index is causing the error
-#print(df[df['length'].isnull()])
-#df = df.drop(73676)
-####
+	min_size = str(args.lower)
+	max_size = str(args.upper)
+	sv_info_file = args.pop_vcf_file
 
-df['length'] = df['length'].astype(int)
-df['length'].value_counts()
-t_rows = df.query('length >= ' + min_size)
-t_rows = t_rows.query('length <=  ' + max_size)
-t = t_rows['length'].value_counts()
+	df = pd.read_csv(sv_info_file, sep='\t', lineterminator='\n')
+	df.columns = ['chrom','start','end','length','seq']
 
-### Use this to print the top N frequencies (helps when trying to narrow down a range)
-#print(t[0:20])
-#mean = t.sum()
-#print(mean)
-###
+	df['length'].value_counts()
+	df = df[df['length']!='.']
 
-plt.hist(t_rows['length'], density=False, bins=len(t))
-#plt.yscale('log')
-plt.ylabel('log(Frequency)')
-plt.xlabel('Insertion Size (bp)')
-plt.show()
+	#### if you get NaN error run this first line and then delete whatever index is causing the error
+	#print(df[df['length'].isnull()])
+	#df = df.drop(73676)
+	####
+
+	df['length'] = df['length'].astype(int)
+	df['length'].value_counts()
+	t_rows = df.query('length >= ' + min_size)
+	t_rows = t_rows.query('length <=  ' + max_size)
+	t = t_rows['length'].value_counts()
+
+	### Use this to print the top N frequencies (helps when trying to narrow down a range)
+	#print(t[0:20])
+	#mean = t.sum()
+	#print(mean)
+	###
+
+	plt.hist(t_rows['length'], density=False, bins=len(t))
+	#plt.yscale('log')
+	plt.ylabel('log(Frequency)')
+	plt.xlabel('Insertion Size (bp)')
+	plt.show()
+
+if __name__ == '__main__':
+	main()
