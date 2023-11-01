853f6af4341f1bd6487376301876049f0c660faf
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-11-01 14:08:23 -0600

    removed unused code

--- src/output_helper.py
+++ src/output_helper.py
@@ -1,80 +1,56 @@
-import argparse
-from optparse import OptionParser
+from argparse import ArgumentParser
 import pandas as pd
-import os
-import json
-from subprocess import Popen, PIPE
-import getpass
-import yaml
-import re
+import os.path
 
-parser = OptionParser()
+def parse_args():
+    parser = ArgumentParser(description = "Process some arguments")
+    parser.add_argument('-s', '--species', required = True, help = "species name")
+    parser.add_argument('-l', '--lower', required = True, help = "lower bound")
+    parser.add_argument('-u', '--upper', required = True, help = "upper bound")
+    return parser.parse_args()
 
-parser.add_option("-s",
-	dest="species",
-	help="species name")
+def main():
+    args = parse_args()
+    species = args.species
+    low, high = args.lower, args.upper
 
-parser.add_option("-l",
-	dest="lower",
-	help="")
+    annotated_file = os.path.join('output', species, f'peak_{low}-{high}', f'{species}_{low}-{high}_gene_annotate.txt')
 
-parser.add_option("-u",
-	dest="upper",
-	help="")
+    # Read the BED file into a DataFrame
+    df = pd.read_csv(annotated_file, sep="\t", header=None, names=["Chrom", "start", "end", "sequence", "sampleID", "gene", "type"])
 
-(options, args) = parser.parse_args()
+    # Sort the DataFrame
+    df = df.sort_values(by=["Chrom", "start", "end"])
 
-config_file = 'config_'+options.species + '.yaml'
+    # Merge overlapping rows
+    merged_rows = []
+    current_row = df.iloc[0]
+    max_seq_len = len(current_row["sequence"])
+    sample_ids = [current_row["sampleID"]]
 
-with open(config_file, 'r') as stream:
-    try:
-        data = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+    for _, row in df.iloc[1:].iterrows():
+        if row["Chrom"] == current_row["Chrom"] and row["start"] <= current_row["end"]:
+            # Adjust end position based on sequence length
+            end_pos = current_row["start"] + max_seq_len
+            current_row["end"] = min(end_pos, row["end"])
+            sample_ids.append(row["sampleID"])
+            max_seq_len = max(max_seq_len, len(row["sequence"]))
+        else:
+            current_row["sampleID"] = ",".join(sample_ids)
+            merged_rows.append(current_row)
+            current_row = row
+            max_seq_len = len(row["sequence"])
+            sample_ids = [row["sampleID"]]
 
-low = options.lower
-high = options.upper
-species =options.species
-print(species)
-data_dir = data['data_directory']
+    # Handle the last row
+    current_row["sampleID"] = ",".join(sample_ids)
+    merged_rows.append(current_row)
 
-annotated_file="output/"+options.species+"/peak_"+low+"-"+high+"/"+species+"_"+low+"-"+high+"_gene_annotate.txt"
-#loci_annotated_file="output/"+options.species+"/peak_"+low+"-"+high+"/"+species+"_"+low+"-"+high+"_gtf_loci.txt"
-#pop_vcf_file="output/"+options.species+"/peak_"+low+"-"+high+"/"+species+"_"+low+"-"+high+"_pop_vcf.txt"
+    # Convert to a new DataFrame
+    merged_df = pd.DataFrame(merged_rows)
+    out_file = "output/"+species+"/peak_"+low+"-"+high+"/"+species+"_"+low+"-"+high+"_genes_merged.txt"
+    # Write to a new BED file
+    merged_df.to_csv(out_file, sep="\t", header=False, index=False)
 
-
-# Read the BED file into a DataFrame
-df = pd.read_csv(annotated_file, sep="\t", header=None, names=["Chrom", "start", "end", "sequence", "sampleID", "gene", "type"])
-
-# Sort the DataFrame
-df = df.sort_values(by=["Chrom", "start", "end"])
-
-# Merge overlapping rows
-merged_rows = []
-current_row = df.iloc[0]
-max_seq_len = len(current_row["sequence"])
-sample_ids = [current_row["sampleID"]]
-
-for _, row in df.iloc[1:].iterrows():
-    if row["Chrom"] == current_row["Chrom"] and row["start"] <= current_row["end"]:
-        # Adjust end position based on sequence length
-        end_pos = current_row["start"] + max_seq_len
-        current_row["end"] = min(end_pos, row["end"])
-        sample_ids.append(row["sampleID"])
-        max_seq_len = max(max_seq_len, len(row["sequence"]))
-    else:
-        current_row["sampleID"] = ",".join(sample_ids)
-        merged_rows.append(current_row)
-        current_row = row
-        max_seq_len = len(row["sequence"])
-        sample_ids = [row["sampleID"]]
-
-# Handle the last row
-current_row["sampleID"] = ",".join(sample_ids)
-merged_rows.append(current_row)
-
-# Convert to a new DataFrame
-merged_df = pd.DataFrame(merged_rows)
-out_file = "output/"+options.species+"/peak_"+low+"-"+high+"/"+species+"_"+low+"-"+high+"_genes_merged.txt"
-# Write to a new BED file
-merged_df.to_csv(out_file, sep="\t", header=False, index=False)
+if __name__ == '__main__':
+    main()
