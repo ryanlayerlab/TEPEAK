02b2e9bf656c4bd7d3613fbcacbfcf6ae359ef9b
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-11-06 15:25:29 -0700

    hardcoded version numbers for packages

--- dep/setup.sh
+++ dep/setup.sh
@@ -1,10 +1,10 @@
 conda config --append channels bioconda
 conda config --append channels conda-forge
-conda install -c bioconda bedtools
-conda install -c conda-forge selenium
-conda install -c bioconda -c conda-forge bcftools
-conda install -c bioconda bwa
-conda install -c bioconda samtools
-conda install jq
+conda install -c bioconda bedtools=2.31.0
+conda install -c conda-forge selenium=4.9.0
+conda install -c bioconda -c conda-forge bcftools=1.17
+conda install -c bioconda bwa=0.7.17
+conda install -c bioconda samtools=1.18
+conda install jq=1.6
 pip install -r dep/requirements.txt
 fastq-dump > /dev/null 2>&1 || export PATH=$PATH:$PWD/$(ls | grep "sratoolkit")/bin
