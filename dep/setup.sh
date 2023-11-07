ee4848a2691e1048ef195c11322403f7c6b03eb6
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-11-07 10:36:52 -0700

    removed jq

--- dep/setup.sh
+++ dep/setup.sh
@@ -5,6 +5,5 @@ conda install -c conda-forge selenium=4.9.0
 conda install -c bioconda -c conda-forge bcftools=1.17
 conda install -c bioconda bwa=0.7.17
 conda install -c bioconda samtools=1.18
-conda install jq=1.6
 pip install -r dep/requirements.txt
 fastq-dump > /dev/null 2>&1 || export PATH=$PATH:$PWD/$(ls | grep "sratoolkit")/bin
