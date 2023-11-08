5c54a859943acce4aa443faf3a134e88ed5039d5
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-11-08 15:57:11 -0700

    removed dep/

--- dep/setup.sh
+++
@@ -1,9 +0,0 @@
-conda config --append channels bioconda
-conda config --append channels conda-forge
-conda install -c bioconda bedtools=2.31.0
-conda install -c conda-forge selenium=4.9.0
-conda install -c bioconda -c conda-forge bcftools=1.17
-conda install -c bioconda bwa=0.7.17
-conda install -c bioconda samtools=1.18
-pip install -r dep/requirements.txt
-fastq-dump > /dev/null 2>&1 || export PATH=$PATH:$PWD/$(ls | grep "sratoolkit")/bin
