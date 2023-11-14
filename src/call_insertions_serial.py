94d7c9326781883e80f42b9772f12c0c73f1f146
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-11-14 11:02:53 -0700

    call_insertions.serial.py snakemake script

--- src/call_insertions_serial.py
+++ src/call_insertions_serial.py
@@ -1,21 +1,13 @@
-from argparse import ArgumentParser
-import os, subprocess, yaml
-
-def parse_args():
-    parser = ArgumentParser(description = "Process and run insurveyor.py on .bam files")
-    parser.add_argument('-s', '--species', required = True, help = "Species name")
-    return parser.parse_args()
+import os, subprocess
 
 def main(): 
-    species = parse_args().species
-    os.makedirs(f'output/{species}', exist_ok = True)    
+    threads = snakemake.params.threads
+    reference_dir = snakemake.input.ref
+    species_dir = snakemake.params.species_dir
+    sra_file = snakemake.input.sample_file
+    output_dir = snakemake.params.output_dir
+    os.makedirs(output_dir, exist_ok = True)    
 
-    with open(f'configs/config_{species}.yaml') as config_file: 
-        config_file = yaml.safe_load(config_file)
-    
-    threads = config_file.get('threads')
-    data_dir = config_file.get('data_directory')
-    reference_dir = os.path.join(data_dir, species, f'{species}.fa')
     cmd = ['insurveyor.py', 
             '--threads', str(threads), 
             'BAM_FILE', 
@@ -23,15 +15,14 @@ def main():
             reference_dir
         ]
     
-    sra_file = os.path.join(data_dir, species, f'{species}_samples.txt')
     with open(sra_file) as sra_file: 
         for line in sra_file: 
             sra_example = line.strip()
-            output_dir = os.path.join('output', species, sra_example)
-            bam_file = os.path.join(data_dir, species, f'{sra_example}.bam')
+            work_dir = os.path.join(output_dir, sra_example)
+            bam_file = os.path.join(species_dir, f'{sra_example}.bam')
 
-            os.makedirs(output_dir, exist_ok = True)
-            cmd[3], cmd[4] = bam_file, output_dir 
+            os.makedirs(work_dir, exist_ok = True)
+            cmd[3], cmd[4] = bam_file, work_dir 
             subprocess.run(cmd)
 
 if __name__ == '__main__':
