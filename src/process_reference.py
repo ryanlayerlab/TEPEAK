00b5a28605becf40a3b91de616865d1d1b4da065
Jishnu Raychaudhuri <jira5329@colorado.edu>
2023-11-14 11:02:41 -0700

    process_reference.py snakemake script

--- src/process_reference.py
+++ src/process_reference.py
@@ -1,30 +1,20 @@
-from  argparse import ArgumentParser
-import subprocess, json, yaml
-
-def parse_args():
-    parser = ArgumentParser(description = "Process and move files")
-    parser.add_argument('-f', '--filename', required = True, help = "zipped genome filepath")
-    parser.add_argument('-s', '--species', required = True, help = "species name")
-    return parser.parse_args()
+import subprocess, json
 
 def main():
-    args = parse_args()
-    filename, species = args.filename, args.species
-    
-    # unzip the file and auto input 'no' to replace README prompt
-    subprocess.run(f'echo "n" | unzip {filename}', shell = True)
-    with open('ncbi_dataset/data/dataset_catalog.json') as dataset_catalog, open(f'configs/config_{species}.yaml') as config_file: 
+    subprocess.run(f'echo "n" | unzip {snakemake.input.ref}', shell = True)
+    species = snakemake.params.species
+    species_dir = snakemake.params.species_dir
+
+    with open('ncbi_dataset/data/dataset_catalog.json') as dataset_catalog: 
         parsed_dataset = json.load(dataset_catalog)
-        config_file = yaml.safe_load(config_file)
     
-    data_dir = config_file['data_directory']
     fa_filepath = f"ncbi_dataset/data/{parsed_dataset['assemblies'][2]['files'][0]['filePath']}"
     subprocess.run(
         f"""
-        cp {fa_filepath} {data_dir}/{species}/{species}.fa
+        cp {fa_filepath} {species_dir}/{species}.fa
         echo "File has been moved and renamed to {species}.fa"
         rm -r ncbi_dataset
-        cd {data_dir}/{species}
+        cd {species_dir}
         samtools faidx {species}.fa
         bwa index -p {species} {species}.fa
         """, shell = True
